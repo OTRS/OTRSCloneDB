786e3e90b0468bc31445aa134b8b1a37ade863cf
Manuel Hecht <manuel.hecht@otrs.com>
2017-10-09 20:56:00 +0200

    Tidied.

--- Kernel/System/CloneDB/Driver/Base.pm
+++ Kernel/System/CloneDB/Driver/Base.pm
@@ -1,6 +1,5 @@
 # --
-# Kernel/System/CloneDB/Driver/Base.pm - Clone DB backend functions
-# Copyright (C) 2001-2015 OTRS AG, http://otrs.com/
+# Copyright (C) 2001-2017 OTRS AG, http://otrs.com/
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
