d5cc6fc8f8b177de3307d8103e710e967816358c
Manuel Hecht <manuel.hecht@otrs.com>
2020-01-05 18:28:51 +0100

    Updated copyright.

--- bin/otrs.CloneDB.pl
+++ bin/otrs.CloneDB.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl
 # --
-# Copyright (C) 2001-2019 OTRS AG, https://otrs.com/
+# Copyright (C) 2001-2020 OTRS AG, https://otrs.com/
 # --
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -102,7 +102,7 @@ exit 0;
 sub _Help {
     print <<EOF;
 $0 migrate OTRS databases
-Copyright (C) 2001-2019 OTRS AG, https://otrs.com/
+Copyright (C) 2001-2020 OTRS AG, https://otrs.com/
 
 This script clones an OTRS database into an empty target database, even
 on another database platform. It will dynamically get the list of tables in the
